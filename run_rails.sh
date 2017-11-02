6e2164232fc327856ed07d637c816ae60ae3ad59
Colin Wu <colin@colinwu.ca>
2017-11-02 12:58:50 -0400

    Cosmetic adjustment of my_pm_list display; update run_rails.sh to reflect new ruby version

--- run_rails.sh
+++ run_rails.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /usr/local/rvm/environments/ruby-2.2.4@PM-Planner
+source /usr/local/rvm/environments/ruby-2.3.3@PM-Planner
 cd /home/wucolin/apps/PMPlanner/current
-env RAILS_ENV=production /usr/local/rvm/gems/ruby-2.2.4@PM-Planner/bin/rails r $1 $2 $3 $4 $5
+env RAILS_ENV=production /usr/local/rvm/gems/ruby-2.3.3@PM-Planner/bin/rails r $1 $2 $3 $4 $5
 
