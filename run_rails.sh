909626304dfb9b02f002b238b09163b054523a72
Colin Wu <colin@colinwu.ca>
2018-04-11 09:40:57 -0400

    Add month avg to small screen only

--- run_rails.sh
+++ run_rails.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-source /usr/local/rvm/environments/ruby-2.3.3@PM-Planner
+source /usr/local/rvm/environments/ruby-2.4.1@PM-Planner
+source /home/wucolin/.profile
 cd /home/wucolin/apps/PMPlanner/current
-env RAILS_ENV=production /usr/local/rvm/gems/ruby-2.3.3@PM-Planner/bin/rails r $1 $2 $3 $4 $5
+env RAILS_ENV=production /usr/local/rvm/gems/ruby-2.4.1@PM-Planner/bin/rails r $1 $2 $3 $4 $5
 
