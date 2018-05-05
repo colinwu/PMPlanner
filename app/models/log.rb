04c7d5c05f75a9663e8d9f07720091f60dc58cb9
Colin Wu <colin@colinwu.ca>
2018-05-04 21:26:07 -0400

    Make Device verification optional in Log model

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,7 +1,7 @@
 class Log < ApplicationRecord
   
   belongs_to :technician
-  belongs_to :device
+  belongs_to :device, optional: true
   
   validates :message, presence: true
   validates :technician_id, numericality: { greater_than: 0 }, allow_nil: true
