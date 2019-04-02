ffecbd2b8b134a8a67f7c0e8529e32989ff4b242
Colin Wu <colin@colinwu.ca>
2019-04-02 08:28:51 -0400

    Fixed bug in log controller

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -11,16 +11,20 @@ class LogsController < ApplicationController
       @search_params = params[:search].permit(:tech, :device, :message).to_h
     end
     search_ar = ['place_holder']
+    join_tech = false;
+    join_dev = false;
     where_ar = []
     if @search_params
       unless @search_params['tech'].nil? or @search_params['tech'].blank?
         search_ar <<  @search_params[:tech]
         search_ar <<  @search_params[:tech]
         where_ar << "(technicians.first_name regexp ? or technicians.last_name regexp ?)"
+        join_tech = true;
       end
       unless  @search_params['device'].nil? or  @search_params['device'].blank?
         search_ar <<  @search_params[:device]
         where_ar << "devices.crm_object_id regexp ?"
+        join_dev = true;
       end
       unless  @search_params['message'].nil? or  @search_params['message'].blank?
         search_ar <<  @search_params[:message]
@@ -31,7 +35,15 @@ class LogsController < ApplicationController
     if search_ar[0] == ''
       @logs = Log.order("created_at desc").page(params[:page])
     else
-      @logs = Log.joins(:technician, :device).where(search_ar).order("created_at desc").page(params[:page])
+      if (join_tech and join_dev)
+        @logs = Log.joins(:technician, :device).where(search_ar).order("created_at desc").page(params[:page])
+      elsif (join_tech and not join_dev)
+        @logs = Log.joins(:technician).where(search_ar).order("created_at desc").page(params[:page])
+      elsif (not join_tech and join_dev)
+        @logs = Log.joins(:device).where(search_ar).order("created_at desc").page(params[:page])
+      else
+        @logs = Log.where(search_ar).order("created_at desc").page(params[:page])
+      end
     end
     respond_with(@logs)
   end
