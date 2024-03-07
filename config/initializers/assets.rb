d9c5f08baf20cb3d799e1b9544e18bb7bfca3461
Colin Wu <colin@colinwu.ca>
2024-03-07 09:48:23 -0500

    still trying...

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,10 +7,10 @@ Rails.application.config.assets.version = '2.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.paths << '/usr/local/rvm/gems/ruby-3.0.0@PM-Planner-Rails7/gems/bootstrap-sass-3.3.7/assets/javascripts'
-Rails.application.config.assets.paths << '/usr/local/rvm/gems/ruby-3.0.0@PM-Planner-Rails7/gems/bootstrap-sass-3.3.7/assets/stylesheets'
-Rails.application.config.assets.paths << '/usr/local/rvm/gems/ruby-3.0.0@PM-Planner-Rails7/gems/bootstrap-sass-3.3.7/assets/images'
-Rails.application.config.assets.paths << '/usr/local/rvm/gems/ruby-3.0.0@PM-Planner-Rails7/gems/bootstrap-sass-3.3.7/assets/fonts'
+Rails.application.config.assets.paths << '/usr/local/rvm/gems/ruby-3.0.0@PM-Planner-Rails7/gems/bootstrap-sass-3.4.1/assets/javascripts'
+Rails.application.config.assets.paths << '/usr/local/rvm/gems/ruby-3.0.0@PM-Planner-Rails7/gems/bootstrap-sass-3.4.1/assets/stylesheets'
+Rails.application.config.assets.paths << '/usr/local/rvm/gems/ruby-3.0.0@PM-Planner-Rails7/gems/bootstrap-sass-3.4.1/assets/images'
+Rails.application.config.assets.paths << '/usr/local/rvm/gems/ruby-3.0.0@PM-Planner-Rails7/gems/bootstrap-sass-3.4.1/assets/fonts'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
